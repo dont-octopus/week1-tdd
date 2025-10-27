7719068e63915a11e08951410736f114d094eb94
yulia <cyy903@daum.net>
2025-10-27 23:27:53 +0900

    포인트 충전 성공 테스트 - RED

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -0,0 +1,33 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointServiceTest {
+
+    @Test
+    @DisplayName("포인트 충전 성공 테스트")
+    void charge_point_success() {
+
+        // Given
+        UserPointTable userPointTable = new UserPointTable();
+        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+        PointService pointService = new PointService(userPointTable, pointHistoryTable);
+
+        long userId = 1L;
+        long amount = 5000L;
+
+        // When
+        UserPoint chargePoint = pointService.chargePoint(userId, amount);
+
+        // Then
+        assertEquals(amount, chargePoint.point());
+
+
+    }
+}
