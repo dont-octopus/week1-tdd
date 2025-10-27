4a1bddc0da4c3667ce47c49d5b44e46c61e3032f
yulia <cyy903@daum.net>
2025-10-28 01:41:55 +0900

    포인트 충전 실패 테스트 (RED) - 충전금액 입력값이 음수인 경우

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PointServiceTest {
 
@@ -29,6 +30,27 @@ public class PointServiceTest {
 
         // Then
         assertEquals(amount, chargePoint.point());
+    }
+
+    @Test
+    @DisplayName("포인트 충전 실패 테스트 - 입력금액이 음수인 경우")
+    void charge_point_fail_if_minus() {
+
+        // Given
+        UserPointTable userPointTable = new UserPointTable();
+        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+
+        PointRepository pointRepository = new PointRepository(userPointTable, pointHistoryTable);
+        PointService pointService = new PointService(pointRepository);
+
+        long userId = 1L;
+        long amount = -5000L;
+
+        // When & Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            pointRepository.save(userId, amount);
+        });
+
 
 
     }
